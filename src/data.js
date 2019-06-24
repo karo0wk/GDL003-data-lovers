5074a3f1148c7c4c0393eba83ce1a31a9e5c7bc7
LibertaAlonso <akerfeldt.lml@gmail.com>
2019-06-24 18:30:45 -0500

    Modificación Json

--- src/data.js
+++ src/data.js
@@ -1,8 +1,26 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
+/*const example = () => {
   return 'example';
 };
 
-window.example = example;
+window.example = example;*/
+
+const filterData = () => {
+  return 'filterData';
+};
+
+const sortData = () => {
+  return 'sortData';
+};
+
+const computeStats = () => {
+  return 'computeStats';
+};
+
+window.example = {
+  filterData,
+  sortData,
+  computeStats
+};
