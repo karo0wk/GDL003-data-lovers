28f000c1762054400f035a68e693fb247a75d171
LibertaAlonso <akerfeldt.lml@gmail.com>
2019-06-25 19:03:44 -0500

    Modificación test cambios

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,41 @@
 require('../src/data.js');
 
-
-describe('example', () => {
+/*describe('example', () => {
   it('is a function', () => {
     expect(typeof example).toBe('function');
   });
 
   it('returns `example`', () => {
     expect(example()).toBe('example');
+  });
+});*/
+
+describe('filterData', () => {
+  it('is a function', () => {
+    expect(typeof example.filterData).toBe('function');
+  });
+
+  it('returns `filterData`', () => {
+    expect(example.filterData()).toBe('filterData');
+  });
+});
+
+describe('sortData', () => {
+  it('is a function', () => {
+    expect(typeof example.sortData).toBe('function');
+  });
+
+  it('returns `sortData`', () => {
+    expect(example.sortData()).toBe('sortData');
+  });
+});
+
+describe('computeStats', () => {
+  it('is a function', () => {
+    expect(typeof example.computeStats).toBe('function');
+  });
+
+  it('returns `computeStats`', () => {
+    expect(example.computeStats()).toBe('computeStats');
   });
 });
