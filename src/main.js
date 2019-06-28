41d3040b6b9cda70128e4fe33a920f85f03ec0d3
LibertaAlonso <akerfeldt.lml@gmail.com>
2019-06-27 21:13:07 -0500

    Motrando datos de .json a HTML

--- src/main.js
+++ src/main.js
@@ -1,34 +1,82 @@
 /*Menú*/
-function changeClass(){
-    let siteNav = document.getElementById('site-nav');
-        siteNav.classList.toggle('site-nav-open');
-    let menuOpen = document.getElementById('menu-toggle');
-        menuOpen.classList.toggle('menu-open');
+function changeClass() {
+	let siteNav = document.getElementById('site-nav');
+	siteNav.classList.toggle('site-nav-open');
+	let menuOpen = document.getElementById('menu-toggle');
+	menuOpen.classList.toggle('menu-open');
 
 }
 
 /*  JSON */
+document.querySelector('#buttonJSON').addEventListener('click', traerDatos);
 
-document.querySelector('#buttonJson').addEventListener('click',traerDatos);
-function traerDatos(){
-  let xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", "https://raw.githubusercontent.com/LibertaAlonso/GDL003-data-lovers/master/src/data/lol/lol.json", true);
-  xmlhttp.send();
+function traerDatos() {
 
-  xmlhttp.onreadystatechange = function(){
+	//console.log('dentro de la función');
+	let xmlhttp = new XMLHttpRequest();
+	xmlhttp.open('GET', 'https://raw.githubusercontent.com/LibertaAlonso/GDL003-data-lovers/master/src/data/lol/lol.json', true);
+	//xmlhttp.open('GET', './data/lol/lol.json', true);
+	xmlhttp.send();
 
-   if (this.readyState == 4 && this.status == 200) {
-     let myObject = JSON.parse(this.responseText);
-     let resuesta= document.querySelector('#respuesta');
-     //respuesta.innerHTML='';
+	xmlhttp.onreadystatechange = function () {
 
-     //console.log("Json parsed data is: " + JSON.stringify(myObject));
+		if (this.readyState == 4 && this.status == 200) {
+			//onsole.log(this.responseText);
 
+			let myObject = JSON.parse(this.responseText);
+			//console.log(myObject);
+			let respuesta = document.querySelector('#respuesta');
+			//respuesta.innerHTML='';
 
-     Object.keys(myObject).forEach(function (key){
-         console.log(myObject[key]);
-     });
+			//console.log("Json parsed data is: " + JSON.stringify(myObject));
 
-   }
- }
+			Object.keys(myObject.data).forEach(function (key) {
+
+				//console.log(myObject.data[key].name);
+
+				respuesta.innerHTML += `
+        <tr>
+        <td><img src="${myObject.data[key].img}"</td>
+        <td>${myObject.data[key].name}</td>
+        <td>${myObject.data[key].version}</td>
+        <td>${myObject.data[key].id}</td>
+        <td>${myObject.data[key].key}</td>
+        <td>${myObject.data[key].title}</td>
+        <td>${myObject.data[key].info.attack}</td>
+        <td>${myObject.data[key].info.defense}</td>
+        <td>${myObject.data[key].info.magic}</td>
+        <td>${myObject.data[key].info.difficulty}</td>
+        </tr>`
+
+			});
+		}
+	}
 }
+
+/*Función filterData*/
+
+/*Función sortData*/
+function Search() {
+            var tableLol = document.getElementById('tableLOL');
+            var search = document.getElementById('textSeach').value.toLowerCase();
+            var cellsOfRow="";
+            var found=false;
+            var compareWith="";
+            for (var i = 1; i < tableLol.rows.length; i++) {
+                cellsOfRow = tableLol.rows[i].getElementsByTagName('td');
+                found = false;
+                for (var j = 0; j < cellsOfRow.length && !found; j++) { compareWith = cellsOfRow[j].innerHTML.toLowerCase(); if (search.length == 0 || (compareWith.indexOf(search) > -1))
+                    {
+                        found = true;
+                    }
+                }
+                if(found)
+                {
+                    tableLol.rows[i].style.display = '';
+                } else {
+                    tableLol.rows[i].style.display = 'none';
+                }
+            }
+        }
+
+/*Función computeStats*/
